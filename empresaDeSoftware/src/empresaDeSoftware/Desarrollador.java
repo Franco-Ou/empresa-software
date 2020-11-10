b464d79694426220f60f8e215917505d0f304347
Franco-Ou <65748031+Franco-Ou@users.noreply.github.com>
2020-11-10 03:20:15 -0300

    Se modificó la clase Desarrollador

--- empresaDeSoftware/src/empresaDeSoftware/Desarrollador.java
+++ empresaDeSoftware/src/empresaDeSoftware/Desarrollador.java
@@ -3,7 +3,10 @@ package empresaDeSoftware;
 import java.util.ArrayList;
 
 public class Desarrollador extends Empleado {
+
+	private Integer proyectosFinalizados = 0;
 	private ArrayList<String> proyectosAsignados = new ArrayList<String>();
+	private Double bono = 2000.00;
 
 	public Desarrollador(Integer dni, Integer idEmpleado, String nombre, Double sueldo, Integer anioDeIngreso) {
 		super(dni, idEmpleado, nombre, sueldo, anioDeIngreso);
@@ -18,6 +21,26 @@ public class Desarrollador extends Empleado {
 
 	public ArrayList<String> getProyectosAsignados() {
 		return proyectosAsignados;
+	}
+
+	public Boolean buscarProyecto(String proyectoBuscado) {
+		for (String proyectoEncontrado : this.proyectosAsignados) {
+			if (proyectoEncontrado.equals(proyectoBuscado)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void finalizarProyecto() {
+		proyectosFinalizados++;
+	}
+
+	// Los desarrolladores tienen un bono de $ 2000 por proyecto finalizado:
+	public Double calcularSueldo(Empleado empleado) {
+		Contaduria micontador = new Contaduria("Equipo A");
+		Double extraPorBono = (Double) (bono * proyectosFinalizados);
+		return micontador.calcularSueldo(empleado) + extraPorBono;
 	}
 
 	@Override
