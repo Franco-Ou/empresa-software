d5828e59d2d0dd31fc41d7bd1834fbf0516843bc
MELINA_FLORES <MELINA.E.FLORES@GMAIL.COM>
2020-12-07 19:36:34 -0300

    Se creo la clase proyecto con todo sus atributos y metodos. Se agrego a gerente la posibilidad de comenzar, cancelar o terminar proyectos. A desarrollador se le agrego el booleano tieneMasDe5ProyectosFinalizados y los métodos: calcularProyectosFinalizados, realizoMasDe5Proyectos. Se agrego calculo de proyecto finalizado para calcular sueldo y se desarrollaron los siguientes tests: calcularGananciaProyecto y calcularSueldoDesarrollador

--- empresaDeSoftware/test/empresaDeSoftware/testEmpresa.java
+++ empresaDeSoftware/test/empresaDeSoftware/testEmpresa.java
@@ -75,4 +75,82 @@ public class testEmpresa {
 		assertEquals(21, empleadoDePrueba4.getDiasDeVacaciones());
 	}
 
+	
+	@Test
+	public void calcularGananciaProyecto() {
+		Proyecto desarrolloSistemaBanco = new Proyecto("BankSystem", 15000.00, 100000.00);
+		Proyecto webECommerce = new Proyecto("TiendaOnline", 8000.00, 50000.00);
+		
+		assertEquals(85000.00, desarrolloSistemaBanco.ganancia(),0);
+		assertEquals(42000.00, webECommerce.ganancia(),0);
+		
+	}
+	
+	@Test
+	public void calcularSueldoDesarrollador() {
+		
+		//El desarrollador 1 tendrá 6 proyectos asignados, pero solo 4 terminados.
+		//El desarrollador 2 tendrá 6 proyectos asignados, y todos terminados
+		
+		Proyecto proyecto1 = new Proyecto("Proyecto 1", 15000.00, 100000.00);
+		Proyecto proyecto2 = new Proyecto("Proyecto 2", 8000.00, 50000.00);
+		Proyecto proyecto3 = new Proyecto("Proyecto 3", 9000.00, 20000.00);
+		Proyecto proyecto4 = new Proyecto("Proyecto 4", 22000.00, 40000.00);
+		Proyecto proyecto5 = new Proyecto("Proyecto 5", 2000.00, 5000.00);
+		Proyecto proyecto6 = new Proyecto("Proyecto 6", 5000.00, 10000.00);
+		Proyecto proyecto7 = new Proyecto("Proyecto 7", 10000.00, 35000.00);
+		Proyecto proyecto8 = new Proyecto("Proyecto 8", 50000.00, 120000.00);
+		
+		Gerente gerente = new Gerente(22909887, 88, "Armando Paredes", 112000.00, 1995);
+		
+		//Si bien no es necesario para el proposito de este test comenzarProyecto(), se incluye para mostrar como seria el flujo
+		gerente.comenzarProyecto(proyecto1);
+		gerente.terminarProyecto(proyecto1);
+		gerente.comenzarProyecto(proyecto2);
+		gerente.terminarProyecto(proyecto2);
+		gerente.comenzarProyecto(proyecto3);
+		gerente.terminarProyecto(proyecto3);
+		gerente.comenzarProyecto(proyecto4);
+		gerente.terminarProyecto(proyecto4);
+		gerente.comenzarProyecto(proyecto5);
+		gerente.terminarProyecto(proyecto5);
+		gerente.comenzarProyecto(proyecto6);
+		gerente.terminarProyecto(proyecto6);
+		//El proyecto 7 es cancelado
+		gerente.comenzarProyecto(proyecto7);
+		gerente.cancelarProyecto(proyecto7);
+		//El poryecto 8 no esta temrinado aun
+		gerente.comenzarProyecto(proyecto8);
+		
+		Desarrollador desarrollador1 = new Desarrollador(30778909, 155, "Juan Perez", 60000.00, 2015);
+		Desarrollador desarrollador2 = new Desarrollador(36445009, 209, "Mariana Arias", 57000.00, 2019);
+		
+		desarrollador1.asignarProyecto(proyecto1);
+		desarrollador1.asignarProyecto(proyecto2);
+		desarrollador1.asignarProyecto(proyecto3);
+		desarrollador1.asignarProyecto(proyecto4);
+		desarrollador1.asignarProyecto(proyecto7);
+		desarrollador1.asignarProyecto(proyecto8);
+		
+		desarrollador2.asignarProyecto(proyecto1);
+		desarrollador2.asignarProyecto(proyecto2);
+		desarrollador2.asignarProyecto(proyecto3);
+		desarrollador2.asignarProyecto(proyecto4);
+		desarrollador2.asignarProyecto(proyecto5);
+		desarrollador2.asignarProyecto(proyecto6);
+		
+		//empleado 1 debiera cobrar $71000 (Sueldo base + $3000 de plus por no tener faltas + $8000 de los 4 proyectos finalizados)
+		//empleado 2 debiera cobrar $72000 (Sueldo base + $3000 de plus por no tener faltas + $12000 de los 6 proyectos finalizados)
+		assertEquals(71000.00, desarrollador1.calcularSueldo(desarrollador1),0);
+		assertEquals(72000.00, desarrollador2.calcularSueldo(desarrollador2),0);
+		
+	
+		
+		
+		
+		
+		
+		
+	}
+	
 }
